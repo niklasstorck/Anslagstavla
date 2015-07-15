8488b9ec7d053c6463368c217101db6b0c7b9c4d
niklasstorck <niklas@family-storck.se>
2015-07-15 21:23:53 +0200

    Version 1.9
    Första på GIT

---
+++ Source/Global.pas
@@ -0,0 +1,12 @@
+unit Global;
+
+interface
+Const c_Version = 'Version 1.9.0.0';
+      c_Year = '2010';
+      c_Copyright = 'Niklas Storck';
+      c_Company = 'Hb Storckholmen';
+      c_mail = 'storckholmen@family-storck.se';
+
+implementation
+
+end.
